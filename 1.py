3f23415a4e3275b637bfa3fb16ce79e15b8ba191
Dmitry Tsybus <tsibous14@gmail.com>
2022-10-18 18:47:45 +0300

    сделано также для шестигранника

--- 1.py
+++ 1.py
@@ -18,31 +18,41 @@ def midpoint(p1, p2):
     return [(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2]
 
 
-vertex = [
-    [0, 0],
-    [1, 0],
-    [1/2, np.cos(np.deg2rad(30))]
-]
+# vertex = [
+#     [0, 0],
+#     [1, 0],
+#     [1/2, np.cos(np.deg2rad(30))]
+# ]
 
+vertex = [
+    [1, 0],
+    [0.5, np.cos(np.deg2rad(30))],
+    [-0.5, np.cos(np.deg2rad(30))],
+    [-1, 0],
+    [-0.5, -np.cos(np.deg2rad(30))],
+    [0.5, -np.cos(np.deg2rad(30))],
+]
 
 pylab.ion()
 fig = plt.figure(figsize=(8, 8))
 ax = fig.add_subplot()
 ax.set_aspect('equal', adjustable='box')
 
-triangle_x = [vertex[0][0], vertex[1][0], vertex[2][0], vertex[0][0]]
-triangle_y = [vertex[0][1], vertex[1][1], vertex[2][1], vertex[0][1]]
+figure_x = [vertex[i][0] for i in range(len(vertex))]
+figure_x.append(vertex[0][0])
+figure_y = [vertex[i][1] for i in range(len(vertex))]
+figure_y.append(vertex[0][1])
 
-pylab.plot(triangle_x, triangle_y, marker="o", c="r")
+pylab.plot(figure_x, figure_y, marker="o", c="r")
 pylab.draw()
 
 x_rand = random.random()
-point = point_in_triangle(vertex[0], vertex[1], vertex[2])
+point = [0.2, 0.2]
 plt.plot([point[0]], [point[1]], marker=".", c="k", markersize=1)
 plt.draw()
 
 for i in range(100000):
-    sel_vertex = random.choice([0, 1, 2])
+    sel_vertex = random.choice([x for x in range(len(vertex))])
     sel_vertex_coord = vertex[sel_vertex]
     point = midpoint(sel_vertex_coord, point)
 
